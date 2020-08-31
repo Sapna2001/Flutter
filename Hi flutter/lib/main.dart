ddb841ea6054d91f19c6e39b7a699f309895b858
Sapna2001 <harisapnanair@gmail.com>
2020-08-31 16:21:24 +0530

    Files Added

--- Hi flutter/lib/main.dart
+++ Hi flutter/lib/main.dart
@@ -14,9 +14,34 @@ class homePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-        body:Container(
-          child: Center(child: Text("Hi Flutter")),
-        )
+        body:Center(
+          child: Container(
+            width: 100,
+            height: 100,
+            padding: const EdgeInsets.all(10),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black,
+                  blurRadius: 10,
+                  spreadRadius: 10,
+                  offset: Offset(2.0,5.0)
+                )
+              ],
+              gradient: LinearGradient(colors: [Colors.green,Colors.yellowAccent])
+            ),
+            child: Text("I am a box",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.red,
+              fontWeight: FontWeight.bold,
+              fontSize: 25
+            )),
+          ),
+        ),
     );
   }
 }
