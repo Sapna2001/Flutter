2e3abdc389190f8a38fe5470ddac73f605d8ff62
Sapna2001 <harisapnanair@gmail.com>
2020-09-13 23:53:24 +0530

    files added

--- tiles/lib/tiles.dart
+++ tiles/lib/tiles.dart
@@ -11,12 +11,32 @@ class _TilesState extends State<Tiles> {
   @override
   Widget build(BuildContext context) {
    return Scaffold(
+     backgroundColor: Colors.red,
      body: ListView.builder(
+         padding: const EdgeInsets.all(25),
          itemCount: list.student.length,
          itemBuilder: (BuildContext context,index) {
-            return ListTile(
-              title: Text('${list.listName(index)}'),
-              subtitle: Text('${list.listRollNo(index)}') ,
+            return Container(
+              margin: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black,
+                        blurRadius: 2,
+                        spreadRadius: 2,
+                        offset: Offset(2.0,3.0),
+                    )
+                  ],
+                  gradient: LinearGradient(colors: [Colors.lightBlue,Colors.white,Colors.lightBlue])
+              ),
+              child: ListTile(
+                  title: Text('${list.listName(index)}',
+                      style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold, color: Colors.black87)),
+                  subtitle: Text('${list.listRollNo(index)}',
+                      style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.black) ,
+              ),
+            )
             );
          }
      ),
