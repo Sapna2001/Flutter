6a24e777de16bba453c24126bdd7d317fa9583cf
Sapna2001 <harisapnanair@gmail.com>
2020-10-13 19:10:52 +0530

    Love Calculator added

--- Rolling Dice/lib/main.dart
+++ Rolling Dice/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:rolling_Dice/homeScreen.dart';
-import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'package:page_transition/page_transition.dart';
+//import 'package:animated_splash_screen/animated_splash_screen.dart';
+//import 'package:page_transition/page_transition.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,13 +10,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner:false,
-      home: AnimatedSplashScreen(
-        splash: 'assets/images/splash.png',
-        nextScreen: HomeScreen(),
-        splashTransition: SplashTransition.rotationTransition,
-        pageTransitionType: PageTransitionType.scale,
-      )
-//       home: HomeScreen(),
+//      home: AnimatedSplashScreen(
+//        splash: 'assets/images/splash.png',
+//        nextScreen: HomeScreen(),
+//        splashTransition: SplashTransition.rotationTransition,
+//        pageTransitionType: PageTransitionType.scale,
+//      )
+       home: HomeScreen(),
     );
   }
 }
